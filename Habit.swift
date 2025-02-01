1731a95d8fec4ebe2bf61c1f372f2b465812cb18
Praneeth-Kattamuri <kattamuri.k@northeastern.edu>
2025-02-01 16:01:53 -0500

    Deleted all files created on swift playgrounds, starting afresh on xcode

--- Habit.swift
+++
@@ -1,9 +0,0 @@
-import Foundation
-
-struct Habit: Identifiable, Codable {
-    var id = UUID()
-    var name: String
-    var reminderTime: Date
-    var isCompleted: Bool = false
-    var lastUpdated: Date = Date()
-}
